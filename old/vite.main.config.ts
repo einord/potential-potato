0b1aafacf83179268afa2d2cc9ebf0898fe7521a
Jonatan Nyqvist <jonte@plik.se>
2025-08-13 19:30:44 +0200

    Rebuild application (#15)
    * feat: update Linux build scripts to include deb package support
    
    * Created new application from scratch.
    
    * Removed unused svg icons
    
    * Created release file.
    
    * feat: add RPM packaging support for Linux builds and update dependencies
    
    * feat: add local build script for ARMv7l Linux distribution
    
    * fix: correct indentation in package.json for linux target configuration

--- old/vite.main.config.ts
+++ old/vite.main.config.ts
@@ -4,9 +4,10 @@ import { builtinModules } from 'module';
 // https://vitejs.dev/config
 export default defineConfig({
     build: {
-        outDir: '.vite/build',
+        outDir: 'build',
         ssr: true,
         target: 'node18',
+        emptyOutDir: true,
         lib: {
             entry: 'src/main.ts',
             formats: ['cjs'],
